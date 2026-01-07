80250da2f0cf2988827f420fa5e79cdd0f775afb
husnainsunny36 <145754978+husnainsunny36@users.noreply.github.com>
2026-01-07 15:57:37 +0000

    Final Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    
+    implementation("com.itextpdf:kernel:7.2.5")
+    implementation("com.itextpdf:layout:7.2.5")
+
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.5")
     
